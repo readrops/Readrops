6c8236d67dee25594cc62e38baf4f5f14cda4c28
Shinokuni <shinokuni@openserver.fr>
2019-11-09 18:21:45 +0100

    Make simplexml respect opml tags order when serializing

--- readropslibrary/src/main/java/com/readrops/readropslibrary/opml/model/OPML.kt
+++ readropslibrary/src/main/java/com/readrops/readropslibrary/opml/model/OPML.kt
@@ -2,8 +2,10 @@ package com.readrops.readropslibrary.opml.model
 
 import org.simpleframework.xml.Attribute
 import org.simpleframework.xml.Element
+import org.simpleframework.xml.Order
 import org.simpleframework.xml.Root
 
+@Order(elements = ["head", "body"])
 @Root(name = "opml", strict = false)
 data class OPML(@field:Attribute(required = true) var version: String?,
                 @field:Element(required = true) var head: Head?,
