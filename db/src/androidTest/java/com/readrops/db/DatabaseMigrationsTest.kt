c11c9983f2fc91e69c4abe59ec0c31f5dced9ac8
Shinokuni <shinokuni@openserver.fr>
2020-07-19 11:30:15 +0200

    Rename db test class

--- db/src/androidTest/java/com/readrops/db/DatabaseMigrationsTest.kt
+++ db/src/androidTest/java/com/readrops/db/DatabaseMigrationsTest.kt
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith
 import java.io.IOException
 
 @RunWith(AndroidJUnit4::class)
-class DatabaseMigrationsTests {
+class DatabaseMigrationsTest {
 
     private val testDb = "migration-test"
 
