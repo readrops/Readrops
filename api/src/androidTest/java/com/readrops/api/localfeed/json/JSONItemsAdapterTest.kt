ca29c4acb06feb91caa7c4b9abbbdfedad3cbf57
Shinokuni <shinokuni@openserver.fr>
2020-09-15 23:30:56 +0200

    Add adapter for jsonfeed items

---
+++ api/src/androidTest/java/com/readrops/api/localfeed/json/JSONItemsAdapterTest.kt
@@ -0,0 +1,77 @@
+package com.readrops.api.localfeed.json
+
+import android.content.Context
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import com.readrops.api.utils.DateUtils
+import com.readrops.api.utils.ParseException
+import com.readrops.db.entities.Item
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.Types
+import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertNotNull
+import okio.Buffer
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4::class)
+class JSONItemsAdapterTest {
+
+    private val context: Context = InstrumentationRegistry.getInstrumentation().context
+
+    private val adapter = Moshi.Builder()
+            .add(Types.newParameterizedType(List::class.java, Item::class.java), JSONItemsAdapter())
+            .build()
+            .adapter<List<Item>>(Types.newParameterizedType(List::class.java, Item::class.java))
+
+    @Test
+    fun normalCasesTest() {
+        val stream = context.resources.assets.open("localfeed/json/json_feed.json")
+
+        val items = adapter.fromJson(Buffer().readFrom(stream))!!
+        val item = items[0]
+
+        assertEquals(items.size, 10)
+        assertEquals(item.guid, "http://flyingmeat.com/blog/archives/2017/9/acorn_and_10.13.html")
+        assertEquals(item.title, "Acorn and 10.13")
+        assertEquals(item.link, "http://flyingmeat.com/blog/archives/2017/9/acorn_and_10.13.html")
+        assertEquals(item.pubDate, DateUtils.stringToLocalDateTime("2017-09-25T14:27:27-07:00"))
+        assertEquals(item.author, "Author 1")
+        assertNotNull(item.content)
+    }
+
+    @Test
+    fun otherCasesTest() {
+        val stream = context.resources.assets.open("localfeed/json/json_items_other_cases.json")
+
+        val item = adapter.fromJson(Buffer().readFrom(stream))!![0]
+
+        assertEquals(item.description, "This is a summary")
+        assertEquals(item.content, "content_html")
+        assertEquals(item.imageLink, "https://image.com")
+        assertEquals(item.author, "Author 1")
+    }
+
+    @Test
+    fun nullTitleTest() {
+        val stream = context.resources.assets.open("localfeed/json/json_items_required_elements.json")
+
+        Assert.assertThrows("Item title is required", ParseException::class.java) { adapter.fromJson(Buffer().readFrom(stream))!![0] }
+    }
+
+    @Test
+    fun nullLinkTest() {
+        val stream = context.resources.assets.open("localfeed/json/json_items_required_elements.json")
+
+        Assert.assertThrows("Item link is required", ParseException::class.java) { adapter.fromJson(Buffer().readFrom(stream))!![1] }
+    }
+
+    @Test
+    fun nullDateTest() {
+        val stream = context.resources.assets.open("localfeed/json/json_items_required_elements.json")
+
+        Assert.assertThrows("Item date is required", ParseException::class.java) { adapter.fromJson(Buffer().readFrom(stream))!![2] }
+    }
+}
---
+++ api/src/main/java/com/readrops/api/localfeed/json/JSONItemsAdapter.kt
@@ -0,0 +1,116 @@
+package com.readrops.api.localfeed.json
+
+import com.readrops.api.utils.DateUtils
+import com.readrops.api.utils.ParseException
+import com.readrops.api.utils.nextNullableString
+import com.readrops.db.entities.Item
+import com.squareup.moshi.FromJson
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.JsonReader
+import com.squareup.moshi.JsonWriter
+
+class JSONItemsAdapter : JsonAdapter<List<Item>>() {
+
+    override fun toJson(writer: JsonWriter, value: List<Item>?) {
+        // not useful
+    }
+
+    @FromJson
+    override fun fromJson(reader: JsonReader): List<Item> {
+        try {
+            val items = arrayListOf<Item>()
+            reader.beginObject()
+
+            while (reader.hasNext()) {
+                when (reader.nextName()) {
+                    "items" -> parseItems(reader, items)
+                    else -> reader.skipValue()
+                }
+            }
+
+            return items
+        } catch (e: Exception) {
+            throw ParseException(e.message)
+        }
+    }
+
+    private fun parseItems(reader: JsonReader, items: MutableList<Item>) {
+        reader.beginArray()
+
+        while (reader.hasNext()) {
+            reader.beginObject()
+            val item = Item()
+
+            var contentText: String? = null
+            var contentHtml: String? = null
+
+            while (reader.hasNext()) {
+                with(item) {
+                    when (reader.selectName(names)) {
+                        0 -> guid = reader.nextString()
+                        1 -> link = reader.nextString()
+                        2 -> title = reader.nextString()
+                        3 -> contentHtml = reader.nextNullableString()
+                        4 -> contentText = reader.nextNullableString()
+                        5 -> description = reader.nextNullableString()
+                        6 -> imageLink = reader.nextNullableString()
+                        7 -> pubDate = DateUtils.stringToLocalDateTime(reader.nextString())
+                        8 -> author = parseAuthor(reader) // jsonfeed 1.0
+                        9 -> author = parseAuthors(reader) // jsonfeed 1.1
+                    }
+                }
+            }
+
+            validateItem(item)
+            item.content = if (contentHtml != null) contentHtml else contentText
+
+            reader.endObject()
+            items += item
+        }
+
+        reader.endArray()
+    }
+
+    private fun parseAuthor(reader: JsonReader): String? {
+        var author: String? = null
+        reader.beginObject()
+
+        while (reader.hasNext()) {
+            when (reader.nextName()) {
+                "name" -> author = reader.nextNullableString()
+                else -> reader.skipValue()
+            }
+        }
+
+        reader.endObject()
+        return author
+    }
+
+    /**
+     * Returns the first author of the array
+     */
+    private fun parseAuthors(reader: JsonReader): String? {
+        val authors = arrayListOf<String?>()
+        reader.beginArray()
+
+        while (reader.hasNext()) {
+            authors.add(parseAuthor(reader))
+        }
+
+        reader.endArray()
+        return if (authors.filterNotNull().isNotEmpty()) authors.filterNotNull().first() else null
+    }
+
+    private fun validateItem(item: Item) {
+        when {
+            item.title == null -> throw ParseException("Item title is required")
+            item.link == null -> throw ParseException("Item link is required")
+            item.pubDate == null -> throw ParseException("Item date id required")
+        }
+    }
+
+    companion object {
+        val names: JsonReader.Options = JsonReader.Options.of("id", "url", "title", "content_html", "content_text",
+                "summary", "image", "date_published", "author", "authors")
+    }
+}
