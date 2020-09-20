6a1ddaeabb63ac5325037ef2a8bfe1dcc9a7fa40
Shinokuni <shinokuni@openserver.fr>
2020-09-20 18:12:05 +0200

    Use LocalRSSDataSource isUrlRSSResource

--- api/src/main/java/com/readrops/api/localfeed/LocalRSSHelper.kt
+++ api/src/main/java/com/readrops/api/localfeed/LocalRSSHelper.kt
@@ -1,18 +1,14 @@
 package com.readrops.api.localfeed
 
-import com.readrops.api.utils.UnknownFormatException
 import java.io.InputStream
 import java.util.regex.Pattern
 
 object LocalRSSHelper {
 
     private const val RSS_DEFAULT_CONTENT_TYPE = "application/rss+xml"
-    private const val RSS_TEXT_CONTENT_TYPE = "text/xml"
-    private const val RSS_APPLICATION_CONTENT_TYPE = "application/xml"
     private const val ATOM_CONTENT_TYPE = "application/atom+xml"
     private const val JSONFEED_CONTENT_TYPE = "application/feed+json"
     private const val JSON_CONTENT_TYPE = "application/json"
-    private const val HTML_CONTENT_TYPE = "text/html"
 
     private const val RSS_2_REGEX = "rss.*version=\"2.0\""
 
@@ -26,8 +22,7 @@ object LocalRSSHelper {
             RSS_DEFAULT_CONTENT_TYPE -> RSSType.RSS_2
             ATOM_CONTENT_TYPE -> RSSType.ATOM
             JSON_CONTENT_TYPE, JSONFEED_CONTENT_TYPE -> RSSType.JSONFEED
-            RSS_TEXT_CONTENT_TYPE, RSS_APPLICATION_CONTENT_TYPE, HTML_CONTENT_TYPE -> RSSType.UNKNOWN
-            else -> throw UnknownFormatException("Unknown content type : $contentType")
+            else -> RSSType.UNKNOWN
         }
     }
 
@@ -37,27 +32,21 @@ object LocalRSSHelper {
     fun getRSSContentType(content: InputStream): RSSType {
         val stringBuffer = StringBuffer()
         val reader = content.bufferedReader()
+        var type = RSSType.UNKNOWN
 
-        var currentLine = reader.readLine()
-        while (currentLine != null) {
-            stringBuffer.append(currentLine)
+        // we get the first 10 lines which should be sufficient to get the type,
+        // otherwise iterating over the whole file could be too slow
+        for (i in 0..9) stringBuffer.append(reader.readLine())
 
-            if (Pattern.compile(RSS_2_REGEX).matcher(stringBuffer.toString()).find()) {
-                reader.close()
-                content.close()
-
-                return RSSType.RSS_2
-            } else if (Pattern.compile(ATOM_REGEX).matcher(stringBuffer.toString()).find()) {
-                reader.close()
-                content.close()
-
-                return RSSType.ATOM
-            }
-
-            currentLine = reader.readLine()
+        if (Pattern.compile(RSS_2_REGEX).matcher(stringBuffer.toString()).find()) {
+            type = RSSType.RSS_2
+        } else if (Pattern.compile(ATOM_REGEX).matcher(stringBuffer.toString()).find()) {
+            type = RSSType.ATOM
         }
 
-        return RSSType.UNKNOWN
+        reader.close()
+        content.close()
+        return type
     }
 
     enum class RSSType {
