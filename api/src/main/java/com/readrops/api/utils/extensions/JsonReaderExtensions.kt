9189be9d933c505139409109291be0dbf5607aba
Shinokuni <shinokuni@openserver.fr>
2021-12-25 17:34:15 +0100

    Add adapter for Fever folders api call

--- api/src/main/java/com/readrops/api/utils/extensions/JsonReaderExtensions.kt
+++ api/src/main/java/com/readrops/api/utils/extensions/JsonReaderExtensions.kt
@@ -13,3 +13,14 @@ fun JsonReader.nextNonEmptyString(): String {
 
 fun JsonReader.nextNullableInt(): Int? =
         if (peek() != JsonReader.Token.NULL) nextInt() else nextNull()
+
+fun JsonReader.skipField() {
+    skipName()
+    skipValue()
+}
+
+fun JsonReader.skipToEnd() {
+    while (hasNext()) {
+        skipField()
+    }
+}
---
+++ api/src/test/java/com/readrops/api/services/fever/adapters/FeverFoldersAdapterTest.kt
@@ -0,0 +1,31 @@
+package com.readrops.api.services.fever.adapters
+
+import com.readrops.api.TestUtils
+import com.readrops.db.entities.Folder
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.Types
+import okio.Buffer
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class FeverFoldersAdapterTest {
+
+    private val adapter = Moshi.Builder()
+        .add(FeverFoldersAdapter())
+        .build()
+        .adapter<List<Folder>>(Types.newParameterizedType(List::class.java, Folder::class.java))
+
+    @Test
+    fun validFoldersTest() {
+        val stream = TestUtils.loadResource("services/fever/folders.json")
+
+        val folders = adapter.fromJson(Buffer().readFrom(stream))!!
+
+        with(folders[0]) {
+            assertEquals(name, "Libre")
+            assertEquals(remoteId, "4")
+        }
+
+
+    }
+}
