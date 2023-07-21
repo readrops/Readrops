f7d1eea3ec3fe16d77e1d923471a7308b556d956
Shinokuni <shinokuni@openserver.fr>
2023-07-21 22:29:52 +0200

    Add new LocalRSSRepository with some tests

---
+++ appcompose/src/main/java/com/readrops/app/compose/repositories/ARepository.kt
@@ -0,0 +1,56 @@
+package com.readrops.app.compose.repositories
+
+import com.readrops.api.services.SyncResult
+import com.readrops.db.Database
+import com.readrops.db.entities.Feed
+import com.readrops.db.entities.Folder
+import com.readrops.db.entities.account.Account
+
+data class ErrorResult(
+    val values: Map<Feed, Exception>
+)
+
+abstract class ARepository(
+    val database: Database,
+    val account: Account
+) {
+
+    /**
+     * This method is intended for remote accounts.
+     */
+    abstract suspend fun login()
+
+    /**
+     * Global synchronization for the local account.
+     * @param selectedFeeds feeds to be updated
+     * @param onUpdate get synchronization status
+     * @return returns the result of the synchronization used by notifications
+     * and errors per feed if occurred to be transmitted to the user
+     */
+    abstract suspend fun synchronize(
+        selectedFeeds: List<Feed>?,
+        onUpdate: (Feed) -> Unit
+    ): Pair<SyncResult, ErrorResult>
+
+    /**
+     * Global synchronization for remote accounts. Unlike the local account, remote accounts
+     * won't benefit from synchronization status and granular synchronization
+     */
+    abstract suspend fun synchronize(): SyncResult
+
+    abstract suspend fun insertNewFeeds()
+}
+
+abstract class BaseRepository(
+    database: Database,
+    account: Account,
+) : ARepository(database, account) {
+
+    open suspend fun updateFeed(feed: Feed) {}
+
+    open suspend fun deleteFeed(feed: Feed) {}
+
+    open suspend fun addFolder(folder: Folder) {}
+
+    open suspend fun deleteFolder(folder: Folder) {}
+}
---
+++ appcompose/src/main/java/com/readrops/app/compose/repositories/LocalRSSRepository.kt
@@ -0,0 +1,93 @@
+package com.readrops.app.compose.repositories
+
+import com.readrops.api.localfeed.LocalRSSDataSource
+import com.readrops.api.services.SyncResult
+import com.readrops.db.Database
+import com.readrops.db.entities.Feed
+import com.readrops.db.entities.Item
+import com.readrops.db.entities.account.Account
+import org.jsoup.Jsoup
+
+class LocalRSSRepository(
+    private val dataSource: LocalRSSDataSource,
+    database: Database,
+    account: Account
+) : BaseRepository(database, account) {
+
+    override suspend fun login() { /* useless here */
+    }
+
+    override suspend fun synchronize(
+        selectedFeeds: List<Feed>?,
+        onUpdate: (Feed) -> Unit
+    ): Pair<SyncResult, ErrorResult> {
+        val errors = mutableMapOf<Feed, Exception>()
+        val syncResult = SyncResult()
+
+        val feeds = if (selectedFeeds.isNullOrEmpty()) {
+            database.newFeedDao().selectFeeds(account.id)
+        } else selectedFeeds
+
+        for (feed in feeds) {
+            try {
+                val pair = dataSource.queryRSSResource(feed.url!!, null)
+
+                pair?.let {
+                    insertNewItems(it.second, feed)
+                    syncResult.items = it.second
+                }
+            } catch (e: Exception) {
+                errors[feed] = e
+            }
+
+        }
+
+        return Pair(syncResult, ErrorResult(errors))
+    }
+
+    override suspend fun synchronize(): SyncResult = throw NotImplementedError("This method can't be called here")
+
+
+    override suspend fun insertNewFeeds() {
+        /*TODO("Not yet implemented")*/
+    }
+
+    private suspend fun insertNewItems(items: List<Item>, feed: Feed) {
+        items.sortedWith(Item::compareTo) // TODO Check if ordering is useful in this situation
+        val itemsToInsert = mutableListOf<Item>()
+
+        for (item in items) {
+            if (!database.itemDao().itemExists(item.guid!!, feed.accountId)) {
+                if (item.description != null) {
+                    item.cleanDescription = Jsoup.parse(item.description).text()
+                }
+
+                if (item.content != null) {
+                    item.readTime = 0.0
+                } else if (item.description != null) {
+                    item.readTime = 0.0
+                }
+
+                item.feedId = feed.id
+                itemsToInsert += item
+            }
+        }
+
+        database.newItemDao().insert(itemsToInsert)
+    }
+
+    private suspend fun insertFeed(feed: Feed): Feed {
+        if (database.newFeedDao().feedExists(feed.url!!, account.id)) {
+            throw IllegalStateException("Feed already exists for account ${account.accountName}")
+        }
+
+        return feed.apply {
+            accountId = account.id
+            // we need empty headers to query the feed just after, without any 304 result
+            etag = null
+            lastModified = null
+
+            id = database.newFeedDao().insert(this).toInt()
+        }
+    }
+}
