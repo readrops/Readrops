d97da320b4bbedb1e67556f7e425e17e4f84842c
Shinokuni <shinokuni@openserver.fr>
2023-08-15 19:07:55 +0200

    Rewrite FeedColors with some tests

--- appcompose/src/main/java/com/readrops/app/compose/ReadropsApp.kt
+++ appcompose/src/main/java/com/readrops/app/compose/ReadropsApp.kt
@@ -1,14 +1,18 @@
 package com.readrops.app.compose
 
 import android.app.Application
+import coil.ImageLoader
+import coil.ImageLoaderFactory
 import com.readrops.api.apiModule
 import com.readrops.db.dbModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.get
 import org.koin.core.context.startKoin
 import org.koin.core.logger.Level
 
-open class ReadropsApp : Application() {
+open class ReadropsApp : Application(), KoinComponent, ImageLoaderFactory {
 
     override fun onCreate() {
         super.onCreate()
@@ -19,5 +23,12 @@ open class ReadropsApp : Application() {
 
             modules(apiModule, dbModule, composeAppModule)
         }
+    }
+
+    override fun newImageLoader(): ImageLoader {
+        return ImageLoader.Builder(this)
+            .okHttpClient { get() }
+            .crossfade(true)
+            .build()
     }
 }
---
+++ appcompose/src/main/java/com/readrops/app/compose/utils/FeedColors.kt
@@ -0,0 +1,48 @@
+package com.readrops.app.compose.utils
+
+import android.content.Context
+import android.graphics.drawable.BitmapDrawable
+import androidx.annotation.ColorInt
+import androidx.palette.graphics.Palette
+import coil.imageLoader
+import coil.request.ImageRequest
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.get
+
+object FeedColors : KoinComponent {
+
+    suspend fun getFeedColor(feedUrl: String): Int {
+        val context = get<Context>() // TODO maybe call imageLoader directly ? may require some DI changes
+
+        val result = context.imageLoader
+            .execute(
+                ImageRequest.Builder(context)
+                    .data(feedUrl)
+                    .allowHardware(false)
+                    .build()
+            ).drawable as BitmapDrawable
+
+        val palette = Palette.from(result.bitmap).generate()
+
+        val dominantSwatch = palette.dominantSwatch
+        return if (dominantSwatch != null && !isColorTooBright(dominantSwatch.rgb)
+            && !isColorTooDark(dominantSwatch.rgb)) {
+            dominantSwatch.rgb
+        } else 0
+    }
+
+    private fun isColorTooBright(@ColorInt color: Int): Boolean {
+        return getColorLuma(color) > 210
+    }
+
+    private fun isColorTooDark(@ColorInt color: Int): Boolean {
+        return getColorLuma(color) < 40
+    }
+
+    private fun getColorLuma(@ColorInt color: Int): Double {
+        val r = color shr 16 and 0xff
+        val g = color shr 8 and 0xff
+        val b = color shr 0 and 0xff
+        return 0.2126 * r + 0.7152 * g + 0.0722 * b
+    }
+}
