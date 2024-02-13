768522eee6b4f17bb55ffcead2b6ab53d17c5563
Shinokuni <shinokuni@openserver.fr>
2024-02-13 23:23:19 +0100

    Improve small FAB appearance in FeedTab

--- appcompose/src/main/java/com/readrops/app/compose/feeds/FeedTab.kt
+++ appcompose/src/main/java/com/readrops/app/compose/feeds/FeedTab.kt
@@ -16,6 +16,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SmallFloatingActionButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
@@ -182,13 +183,12 @@ object FeedTab : Tab {
             },
             floatingActionButton = {
                 Column {
-                    FloatingActionButton(
+                    SmallFloatingActionButton(
                         modifier = Modifier
                             .padding(
-                                end = 0.dp,
-                                bottom = MaterialTheme.spacing.mediumSpacing
-                            )
-                            .size(40.dp),
+                                start = MaterialTheme.spacing.veryShortSpacing,
+                                bottom = MaterialTheme.spacing.shortSpacing
+                            ),
                         onClick = { viewModel.openDialog(DialogState.AddFolder) }
                     ) {
                         Icon(
