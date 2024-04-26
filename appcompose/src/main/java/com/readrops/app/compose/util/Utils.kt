a3ffde0d73370a42fe5f0a7a259f4ff289eb1ea1
Shinokuni <shinokuni@openserver.fr>
2024-04-26 18:02:36 +0200

    Fix item read time calculation

--- appcompose/src/main/java/com/readrops/app/compose/util/Utils.kt
+++ appcompose/src/main/java/com/readrops/app/compose/util/Utils.kt
@@ -9,7 +9,7 @@ object Utils {
     private const val AVERAGE_WORDS_PER_MINUTE = 250
 
     fun readTimeFromString(value: String): Double {
-        val nbWords = value.split("\\s+").size
+        val nbWords = value.split(Regex("\\s+")).size
         return nbWords.toDouble() / AVERAGE_WORDS_PER_MINUTE
     }
 
