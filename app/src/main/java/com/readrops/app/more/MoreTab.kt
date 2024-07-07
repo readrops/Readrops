016d309d058ced8463eea53321c4cc26f9bf548b
Shinokuni <shinokuni@openserver.fr>
2024-07-07 15:54:26 +0200

    Add github repo, issues and changelog links in MoreTab

--- app/src/main/java/com/readrops/app/more/MoreTab.kt
+++ app/src/main/java/com/readrops/app/more/MoreTab.kt
@@ -1,15 +1,21 @@
 package com.readrops.app.more
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -20,7 +26,9 @@ import cafe.adriel.voyager.navigator.tab.TabOptions
 import com.readrops.app.BuildConfig
 import com.readrops.app.R
 import com.readrops.app.account.selection.adaptiveIconPainterResource
+import com.readrops.app.util.components.IconText
 import com.readrops.app.util.components.SelectableIconText
+import com.readrops.app.util.openUrl
 import com.readrops.app.util.theme.LargeSpacer
 import com.readrops.app.util.theme.MediumSpacer
 import com.readrops.app.util.theme.ShortSpacer
@@ -39,6 +47,7 @@ object MoreTab : Tab {
     @Composable
     override fun Content() {
         val navigator = LocalNavigator.currentOrThrow
+        val context = LocalContext.current
 
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
@@ -63,10 +72,60 @@ object MoreTab : Tab {
 
             ShortSpacer()
 
-            Text(
-                text = "v${BuildConfig.VERSION_NAME} (${BuildConfig.VERSION_CODE})",
+            IconText(
+                text = if (BuildConfig.DEBUG) {
+                    "v${BuildConfig.VERSION_NAME} (${BuildConfig.VERSION_CODE})"
+                } else {
+                    "v${BuildConfig.VERSION_NAME}"
+                },
+                icon = painterResource(id = R.drawable.ic_version),
                 style = MaterialTheme.typography.labelLarge
             )
+
+            ShortSpacer()
+
+            Text(
+                text = stringResource(id = R.string.app_licence),
+                style = MaterialTheme.typography.labelSmall,
+                color = MaterialTheme.colorScheme.onSurfaceVariant
+            )
+
+            ShortSpacer()
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                IconButton(
+                    onClick = { context.openUrl(context.getString(R.string.app_url)) }
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_github),
+                        contentDescription = null,
+                        modifier = Modifier.size(24.dp)
+                    )
+                }
+
+                IconButton(
+                    onClick = { context.openUrl(context.getString(R.string.changelog_url)) }
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_changelog),
+                        contentDescription = null,
+                        modifier = Modifier.size(24.dp)
+                    )
+                }
+
+                IconButton(
+                    onClick = { context.openUrl(context.getString(R.string.app_issues_url)) }
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_bug_report),
+                        contentDescription = null
+                    )
+                }
+            }
 
             LargeSpacer()
 
@@ -86,6 +145,15 @@ object MoreTab : Tab {
                 spacing = MaterialTheme.spacing.mediumSpacing,
                 padding = MaterialTheme.spacing.mediumSpacing,
                 onClick = { navigator.push(AboutLibrariesScreen()) }
+            )
+
+            SelectableIconText(
+                icon = painterResource(id = R.drawable.ic_donation),
+                text = stringResource(id = R.string.make_donation),
+                style = MaterialTheme.typography.titleMedium,
+                spacing = MaterialTheme.spacing.mediumSpacing,
+                padding = MaterialTheme.spacing.mediumSpacing,
+                onClick = { }
             )
         }
     }
--- app/src/main/java/com/readrops/app/util/Extensions.kt
+++ app/src/main/java/com/readrops/app/util/Extensions.kt
@@ -1,7 +1,12 @@
 package com.readrops.app.util
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 fun TextStyle.toDp(): Dp = fontSize.value.dp
+
+fun Context.openUrl(url: String) = startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url)))
