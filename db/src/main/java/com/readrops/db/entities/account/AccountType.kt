edb81f6809800e51bea425f2db2d187cb1779813
Shinokuni <shinokuni@openserver.fr>
2024-08-13 13:59:41 +0200

    Add fever icon

--- db/src/main/java/com/readrops/db/entities/account/AccountType.kt
+++ db/src/main/java/com/readrops/db/entities/account/AccountType.kt
@@ -11,5 +11,5 @@ enum class AccountType(@DrawableRes val iconRes: Int,
     NEXTCLOUD_NEWS(R.drawable.ic_nextcloud_news, R.string.nextcloud_news, AccountConfig.NEXTCLOUD_NEWS),
     //FEEDLY(R.drawable.ic_feedly, R.string.feedly, null),
     FRESHRSS(R.drawable.ic_freshrss, R.string.freshrss, AccountConfig.FRESHRSS),
-    FEVER(R.mipmap.ic_launcher, R.string.fever, AccountConfig.FEVER)
+    FEVER(R.drawable.ic_fever, R.string.fever, AccountConfig.FEVER)
 }
