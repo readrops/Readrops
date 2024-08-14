4803624b33bffb486c3cb5142d6514aa888cd85c
Shinokuni <shinokuni@openserver.fr>
2024-08-14 14:09:13 +0200

    Improve a bit Drawer folder item

--- app/src/main/java/com/readrops/app/timelime/drawer/TimelineDrawer.kt
+++ app/src/main/java/com/readrops/app/timelime/drawer/TimelineDrawer.kt
@@ -20,6 +20,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.readrops.app.R
@@ -63,6 +64,7 @@ fun TimelineDrawer(
                         label = {
                             Text(
                                 text = folder.name!!,
+                                fontWeight = FontWeight.Medium,
                                 maxLines = 1,
                                 overflow = TextOverflow.Ellipsis
                             )
