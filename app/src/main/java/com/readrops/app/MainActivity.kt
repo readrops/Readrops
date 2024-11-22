640744bf6d78434bfff601623b948140d655998c
Shinokuni <shinokuni@openserver.fr>
2024-11-22 18:44:50 +0100

    Fix navigation bar transparency in some screens

--- app/src/main/java/com/readrops/app/MainActivity.kt
+++ app/src/main/java/com/readrops/app/MainActivity.kt
@@ -8,9 +8,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.BottomAppBarDefaults
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.graphics.toArgb
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.lifecycleScope
 import cafe.adriel.voyager.core.annotation.ExperimentalVoyagerApi
@@ -68,7 +70,15 @@ class MainActivity : ComponentActivity(), KoinComponent {
                     useDarkTheme = useDarkTheme
                 ) {
                     enableEdgeToEdge(
-                        statusBarStyle = SystemBarStyle.auto(Color.TRANSPARENT, Color.TRANSPARENT),
+                        statusBarStyle = SystemBarStyle.auto(
+                            lightScrim = Color.TRANSPARENT,
+                            darkScrim = Color.TRANSPARENT,
+                            detectDarkMode = { useDarkTheme }
+                        ),
+                        navigationBarStyle = SystemBarStyle.light(
+                            scrim = BottomAppBarDefaults.containerColor.toArgb(),
+                            darkScrim = BottomAppBarDefaults.containerColor.toArgb()
+                        )
                     )
 
                     Navigator(
